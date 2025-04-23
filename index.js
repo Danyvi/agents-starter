a7190b2e68742ee79d5232ca69294f8b868ab2e7
Danyvi <vinci.dani@gmail.com>
2025-04-23 11:16:54 +0200

    Implement weather and location retrieval functions for activity suggestions

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-import OpenAI from "openai"
 import { openai } from './config.js';
+import { getCurrentWeather, getLocation } from "./tools"
 
+/**
+ * Goal - build an agent that can get the current weather at my current location
+ * and give me some localized ideas of activities I can do.
+ */
 
+const weather = await getCurrentWeather()
+const location = await getLocation()
 
+const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: [
+        {
+            role: "user",
+            content: `Give me a list of activity ideas based on my current location of ${location} and weather of ${weather}`
+        }
+    ]
+})
 
+console.log(response.choices[0].message.content)
